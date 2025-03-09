85b7e8e7e923111d65510b093aeba4d236ecb1d4
Chris <clee@mandalaytech.com>
2025-03-10 08:34:05 +1000

    Helper tests

---
+++ StationStops/src/StationStops/Helpers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StationStops;
+public static class Helpers
+{
+    /// <summary>
+    /// Create a list of adjacent station stops starting at the first found stop. 
+    /// Bails as soon as the next station is express.
+    /// </summary>
+    /// <param name="stations">List of stations to get contiguous stops</param>
+    /// <returns>Returns a list of stations from the first stop, where the next station is a stop.</returns>
+    public static List<Station> FilterAdjacentItems(List<Station> stations)
+    {
+        var lastServedIndex = stations.FindLastIndex(s => s.StationStop);
+        var truncatedStations = stations.Take(lastServedIndex + 1).ToList();
+        if (!truncatedStations.Any()) return stations;
+
+        var firstStop = truncatedStations.First(s => s.StationStop);
+
+        var adjacentStations = new List<Station>();
+        var searchList = stations.Skip(truncatedStations.IndexOf(firstStop) - 1).ToList();
+
+        for (var i = 0; i < searchList.Count; i++)
+        {
+            var isAdjacentTrue = stations[i].StationStop == true && ((i > 0 && stations[i - 1].StationStop) || (i < stations.Count - 1 && stations[i + 1].StationStop));
+
+            if (isAdjacentTrue)
+            {
+                adjacentStations.Add(stations[i]);
+            }
+            else
+            {
+                return adjacentStations;
+            }
+        }
+
+        return adjacentStations;
+    }
+
+}
--- StationStops/src/StationStops/TrainStopService.cs
+++ StationStops/src/StationStops/TrainStopService.cs
@@ -183,7 +183,7 @@ public class TrainStopService
     /// <returns>Train journey segment or null if no contiguous station stops are found</returns>
     public Segment? GetContiguousSegmentFromList(List<Station> stations)
     {
-        var contiguousSection = FilterAdjacentItems(stations);
+        var contiguousSection = Helpers.FilterAdjacentItems(stations);
 
         if (contiguousSection.Any())
         {
@@ -191,40 +191,6 @@ public class TrainStopService
         }
 
         return null;
-    }
-
-    /// <summary>
-    /// Create a list of adjacent station stops starting at the first found stop. 
-    /// Bails as soon as the next station is express.
-    /// </summary>
-    /// <param name="stations">List of stations to get contiguous stops</param>
-    /// <returns>Returns a list of stations from the first stop, where the next station is a stop.</returns>
-    private static List<Station> FilterAdjacentItems(List<Station> stations)
-    {
-        var lastServedIndex = stations.FindLastIndex(s => s.StationStop);
-        var truncatedStations = stations.Take(lastServedIndex + 1).ToList();
-        if (!truncatedStations.Any()) return stations;
-
-        var firstStop = truncatedStations.First(s => s.StationStop);
-
-        List<Station> result = new List<Station>();
-        var searchList = stations.Skip(truncatedStations.IndexOf(firstStop) - 1).ToList();
-
-        for (int i = 0; i < searchList.Count; i++)
-        {
-            bool isAdjacentTrue = stations[i].StationStop == true && ((i > 0 && stations[i - 1].StationStop) || (i < stations.Count - 1 && stations[i + 1].StationStop));
-
-            if (isAdjacentTrue)
-            {
-                result.Add(stations[i]);
-            }
-            else
-            {
-                return result;
-            }
-        }
-
-        return result;
     }
 
     /// <summary>
